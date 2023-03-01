2a1c4d8673e0f4208472a5e52497025c86d02d95
Artem Korytko <a.korytko@freeplay.io>
2023-03-01 21:58:44 +0300

    add board logic

--- Assets/Scripts/Zenject/GameMonoInstaller.cs
+++ Assets/Scripts/Zenject/GameMonoInstaller.cs
@@ -1,12 +1,17 @@
 using Match3;
+//using Match3.Zenject;
 using UnityEngine;
 using Zenject;
 
 public class GameMonoInstaller : MonoInstaller
 {
     [SerializeField] private Element elementPrefab;
+
     public override void InstallBindings()
     {
+        // SignalBusInstaller.Install(Container);
+        // SignalsInstaller.Install(Container);
+        SignalInstaller.Install(Container);
         Container.BindInterfacesAndSelfTo<BoardController>().AsSingle().NonLazy();
         Container.BindFactory<ElementConfigItem, ElementPosition, Element, Element.Factory>().FromComponentInNewPrefab(elementPrefab);
     }
